ad395400211c598a0d64180994b039ac5e5a8c9c
Muhamad Akbar Afriansyah <itsme@akbarafriansyah.my.id>
2022-12-02 13:56:56 +0000

    adding happy cup

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,14 @@
 import logo from "../assets/ttd_2_putih_garis.png"
+import happyCup from "../assets/happy_cup.svg"
 
 const Home = () => {
     return (
         <div className="Home">
             <div className="hello">
-                Hello there... Saya
+                Hello there... I'am
             </div>
             <div className="name">
-                <img className="akbarafriansyahpng" src={logo} alt="" />
+                <img className="akbarafriansyahpng" src={happyCup} alt="" />
             </div>
             <div className="person">
                 And Iam a 
